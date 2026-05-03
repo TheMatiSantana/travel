a5fb8d31f7636f52eae5ff07437f41357a31eb01
TheMatiSantana <santana.matias@gmail.com>
2026-05-02 22:19:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - París 2026
 // Estrategia: cache-first para todo el contenido estático
 
-const CACHE_NAME = 'paris2026-v8';
+const CACHE_NAME = 'paris2026-v11';
 const ASSETS = [
   './',
   './index.html',
